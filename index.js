16f11085867f836f36079db175bf6aa25e7ba804
Suresh Nalimela <suresh.nalimela@realpage.com>
2019-05-12 22:33:56 +0530

    First Sample API

--- index.js
+++ index.js
@@ -6,11 +6,15 @@ const app = express()
 // add some security-related headers to the response
 app.use(helmet())
 
-app.get('*', (req, res) => {
+app.get('/', (req, res) => {
     res.set('Content-Type', 'text/html')
     res.status(200).send(`
         <h1>Hello from App Engine!</h1>
     `)
 })
+
+app.get('/user/:name', (req, res)=> {
+    res.send('<h1>Hello ' + req.params.name);
+});
 
 module.exports = app
