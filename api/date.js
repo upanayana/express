5a3c6348f8dbe2a8c5ff0d9fc27e5b29325030db
Suresh Nalimela <suresh.nalimela@realpage.com>
2019-06-01 20:10:06 +0530

    Mongodb Connection Changes

--- api/date.js
+++ api/date.js
@@ -1,7 +1,23 @@
-const moment = require('moment')
+const moment = require('moment');
+
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://root:root123@cluster0-jccxt.mongodb.net/gearup-sportz?retryWrites=true&w=majority";
+
 
 module.exports = (req, res) => {
-  const currentTime = moment().format('MMMM Do YYYY, h:mm:ss a')
+  const client = new MongoClient(uri, { useNewUrlParser: true });
+  client.connect(err => {
+    if(!err) {
+      const collection = client.db("gearup-sportz").collection("users");
+      
+      collection.findOne({name: 'GearupSportz'}, function (e, i){
+        res.end(JSON.stringify(i));
+      });
 
-  res.end(currentTime)
+    } else {
+      res.end( 'Err Connecting :(');
+    }
+    
+  });
+
 }
